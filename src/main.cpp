8186647b8a23724ae59cf8a156034b9498b0c172
Ahmad A Kazi <niitassin@gmail.com>
2015-05-05 18:04:14 +0530

    Revert "Block Reward Changing"
    This reverts commit fa6f62caf72b5bc0d68d85f0e6520fb42150da3f.

--- src/main.cpp
+++ src/main.cpp
@@ -977,73 +977,73 @@ int64 GetProofOfWorkReward(int nHeight, int64 nFees, uint256 prevHash)
 	{
 		nSubsidy = 75000 * COIN;
 	}
-	else if(nHeight >= 600)
+	else if(nHeight >= 2000)
 	{
 		nSubsidy = 73000 * COIN;
 	}
-	else if(nHeight >= 800)
+	else if(nHeight >= 4000)
 	{
 		nSubsidy = 70000 * COIN;
 	}
-	else if(nHeight >= 1000)
+	else if(nHeight >= 8000)
 	{
 		nSubsidy = 69850 * COIN;
 	}
-	else if(nHeight >= 1200)
+	else if(nHeight >= 12000)
 	{
 		nSubsidy = 67842 * COIN;
 	}
-	else if(nHeight >= 1400)
+	else if(nHeight >= 16000)
 	{
 		nSubsidy = 65951 * COIN;
 	}
-	else if(nHeight >= 1800)
+	else if(nHeight >= 18000)
 	{
 		nSubsidy = 62854 * COIN;
 	}
-	else if(nHeight >= 2400)
+	else if(nHeight >= 24000)
 	{
 		nSubsidy = 51268 * COIN;
 	}
-	else if(nHeight >= 2841)
+	else if(nHeight >= 28418)
 	{
 		nSubsidy = 48968 * COIN;
 	}
-	else if(nHeight >= 3125)
+	else if(nHeight >= 31235)
 	{
 		nSubsidy = 44849 * COIN;
 	}
-	else if(nHeight >= 3489)
+	else if(nHeight >= 34895)
 	{
 		nSubsidy = 43586 * COIN;
 	}
-	else if(nHeight >= 3894)
+	else if(nHeight >= 38964)
 	{
 		nSubsidy = 40000 * COIN;
 	}
-	else if(nHeight >= 4000 && nFees <= 1 * COIN)
+	else if(nHeight >= 40000 && nFees <= 1 * COIN)
 	{
-		nSubsidy = 4000 * COIN;
+		nSubsidy = 39523 * COIN;
 	}
-	else if(nHeight >= 4000 && nFees >= 100 * COIN)
+	else if(nHeight >= 40000 && nFees <= 100 * COIN)
 	{
 		nSubsidy = nFees * 4444444 / 12121212;
-		if(nSubsidy >= 100) {nSubsidy = nFees;}
+		if(nSubsidy >= 1000) {nSubsidy = nFees;}
 	}
-	else if(nHeight >= 4000 && nFees >= 1000 * COIN)
+	else if(nHeight >= 40000 && nFees <= 1000 * COIN)
 	{
 		nSubsidy = nFees * 2222222 / 66666;
 		if(nSubsidy >= 10000) {nSubsidy = nFees;}
 	}
-	else if(nHeight >= 4000 && nFees >= 10000 * COIN)
+	else if(nHeight >= 40000 && nFees <= 10000 * COIN)
 	{
 		nSubsidy = nFees * 8888888 / 3434343434 * 10000;
-		if(nSubsidy >= 10000) {nSubsidy = nFees / 4;}
+		if(nSubsidy >= 10000) {nSubsidy = nFees;}
 	}
-	else if(nHeight >= 4000 && nFees >= 100000 * COIN)
+	else if(nHeight >= 40000 && nFees <= 100000 * COIN)
 	{
 		nSubsidy = nFees * 1111111 / 222222;
-		if(nSubsidy >= 1000000) {nSubsidy = nFees / 8;}
+		if(nSubsidy >= 1000000) {nSubsidy = nFees;}
 	}
 	
 	return nSubsidy + nFees;
